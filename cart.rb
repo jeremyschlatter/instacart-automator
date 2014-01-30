63d21614436c0cae1174df26459eda9b3f3aa42e
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-01-30 08:48:18 +0000

    make checkout work more reliably by waiting for elements to render, work faster by not using sleep statements, and document it better

--- cart.rb
+++ cart.rb
@@ -4,23 +4,25 @@ class Cart < AttributeInitializable
   end
 
   def self.place_order
+    # Open cart dropdown and click checkout button.
     @@browser.div(:id => "cart-in-nav").click
     @@browser.a(:class => "btn-checkout").click
 
-    sleep 1
+    # Delivery page. Use the defaults.
+    # TODO: Add delivery instructions.
+    # TODO: Set delivery time.
+    @@browser.button(:value, /Next/).when_present(5).click
 
-    # Deliver between 6 and 7 pm.
-    #@@browser.select_list(:id, "delivery_option_1").select_value("6-7pm")
+    # Payment page. Use default credit card, add a $2 tip.
+    @@browser.element(:data_tip_amount, '2').when_present(5).click
+    Watir::Wait.until { @@browser.button(:value, /Next/).enabled? }
+    @@browser.button(:value, /Next/).click
 
-    # Submit order.
-    puts @@browser.url, @@browser.title
-    @@browser.button(:value, /Next/).click
-    sleep 2
-    puts @@browser.url, @@browser.title
-    @@browser.element(:data_tip_amount, '2').click
-    puts @@browser.url, @@browser.title
-    @@browser.button(:value, /Next/).click
-    puts @@browser.url, @@browser.title
-    @@browser.button(:value, /Place Order/).click
+    # Replacements page. Leave the default replacements and place the order!
+    @@browser.button(:value, /Place Order/).wait_until_present 5
+
+    # (Except don't actually place the order for now. Just take a screenshot to show we got this far.)
+    @@browser.screenshot.save("/var/screenshots/place-order.png")
+    #@@browser.button(:value, /Place Order/).click
   end
 end
