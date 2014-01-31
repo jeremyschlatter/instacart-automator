062be34f51237a5f055316012a982542ff2e0b7f
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-01-31 01:39:34 +0000

    Add more prints in checkout process, auto-fill delivery instructions and delivery time (with hardcoded values for now), remove one cause of flaky failure.

--- cart.rb
+++ cart.rb
@@ -9,20 +9,20 @@ class Cart < AttributeInitializable
     @@browser.a(:class => "btn-checkout").click
 
     # Delivery page. Use the defaults.
-    # TODO: Add delivery instructions.
-    # TODO: Set delivery time.
+    puts "Filling out delivery page..."
+    @@browser.textarea(:id, "order_special_instructions").when_present(5).set("Please leave groceries at the door if no one is home. Thanks!")
+    @@browser.element(:xpath, "//div[@id='delivery-options-1']/div/li/span").when_present(5).click
+    @@browser.li({:class => "delivery-option", :text => /7-8pm/}).click
     @@browser.button(:value, /Next/).when_present(5).click
 
     # Payment page. Use default credit card, add a $2 tip.
+    puts "Filling out payment page..."
     @@browser.element(:data_tip_amount, '2').when_present(5).click
-    Watir::Wait.until { @@browser.button(:value, /Next/).enabled? }
+    Watir::Wait.until { @@browser.button(:value, /Next/).when_present(5).enabled? }
     @@browser.button(:value, /Next/).click
 
     # Replacements page. Leave the default replacements and place the order!
-    @@browser.button(:value, /Place Order/).wait_until_present 5
-
-    # (Except don't actually place the order for now. Just take a screenshot to show we got this far.)
-    @@browser.screenshot.save("/var/screenshots/place-order.png")
-    #@@browser.button(:value, /Place Order/).click
+    puts "Submitting final confirmation..."
+    @@browser.button(:value, /Place Order/).when_present(5).click
   end
 end
